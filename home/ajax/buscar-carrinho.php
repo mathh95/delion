2bf384d07f56a67a21ac3c7984efed9bc2c94aa8
Vitor Baldacin <vitormatheussb@gmail.com>
2018-08-21 17:48:07 -0300

    Começando a mexer com quantidade no carrinho

--- home/ajax/buscar-carrinho.php
+++ home/ajax/buscar-carrinho.php
@@ -38,24 +38,27 @@ if(count($itens) > 0){
                     <tr>
                         <th>Produto</th>
                         <th>Preço Unitário</th>
+                        <th>Quantidade</th>
                         <th>Foto</th>
                     </tr>
                 </thead>
                 <tbody>
         
                 <?php $total = 0; 
+                $i = 0;
                 foreach($itens as $item): ?>
                     <tr>
                         <td><strong><?=$item['nome']?></strong></td>
                         <td>R$ <?=$item['preco']?></td>
+                        <td><input id="qtdUnidade<?=$i?>" name="quantidade" type="number" value=0 readonly="true"><button id="adicionarUnidade" data-pagina="<?=$i?>" class="btn btn-success">+</button><button id="removerUnidade" data-pagina="<?=$i?>" class="btn btn-danger">-</button></td>
                         <td><img style="width:200px;heigth:150px;" src="../admin/<?=$item['foto']?>"></td>
                     </tr>
-            <?php $total += $item['preco']; endforeach;
+            <?php $i++; $total += $item['preco']; endforeach;
     
     echo "</tbody>
         </table>
         </div>
-        <p>Valor total do pedido: R$".$total."</p>
+        <p id='total'>Valor total do pedido: R$".$total."</p>
         <button class='btn btn-default'>Finalizar pedido</button>";
 
 
@@ -64,3 +67,27 @@ if(count($itens) > 0){
 }
 
 ?>
+
+<script>
+
+    $(document).on("click", "#adicionarUnidade", function(){
+        
+        var acao = "+";
+        var linha = $(this).data('linha');
+        var qtdAtual = $("#qtdUnidade"+linha).val();
+
+        $.ajax({
+            //falta pensar em uma forma de mandar o preço pra outra página...
+            type: 'GET',
+
+            url: 'ajax/quantidade-carrinho.php',
+
+            data: {acao: acao, qtdAtual: qtdAtual},
+
+            success:function(resultado){
+                $("#total").html(resultado)
+            }
+        });
+    });
+
+</script>
---
+++ home/ajax/quantidade-carrinho.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+// ini_set('display_errors', true);
+
+// date_default_timezone_set('America/Sao_Paulo');
+
+
+
+// include_once "../../admin/controler/conexao.php";
+
+// require_once "../controler/controlCarrinho.php";
+
+// require_once "../controler/controlCardapio.php";
+
+// include_once "../lib/alert.php";
+ $id = 0;
+
+if(isset($_GET['id']) && !empty($_GET['id'])){
+    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+    if(!isset($_SESSION['carrinho'])){
+        $_SESSION['carrinho'] = array();
+        // print_r($_SESSION['carrinho']);
+        // exit;
+    }
+
+    // $_SESSION['carrinho'][] = $id;
+
+    if(!in_array($id, $_SESSION['carrinho'], true)){
+        
+        array_push($_SESSION['carrinho'], $id);
+        //essa sessão está sendo startada na index do projeto
+    }
+}
+
+
+
+?>
