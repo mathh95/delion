9cafdda702fff4e10062ddfc32f864c9bbfa0541
Vitor Baldacin <vitormatheussb@gmail.com>
2018-08-30 09:49:39 -0300

    apenas comentando testes

--- home/ajax/buscar-carrinho.php
+++ home/ajax/buscar-carrinho.php
@@ -40,8 +40,8 @@ if(count($itens) > 0){
 
         <script type="text/javascript" src="js/buscar-carrinho.js"></script>
         <h1>Lista de produtos no carrinho</h1>
-        <?php print_r($_SESSION['qtd']); ?>
-        <?php print_r($_SESSION['carrinho']); ?>
+        <?php //print_r($_SESSION['qtd']); ?>
+        <?php //print_r($_SESSION['carrinho']); ?>
         <div class="table-responsive categoria">
             <table class="table table-hover table-condensed">
                 <thead>
