d154902b372467f16768a49d054834b40bd5dae0
Vitor Baldacin <vitormatheussb@gmail.com>
2018-09-27 16:06:20 -0300

    adição do estilo das estrelas da avaliação

--- home/avaliacao.php
+++ home/avaliacao.php
@@ -15,11 +15,17 @@ $tipos = $control->selectAtivo();
 <!DOCTYPE html>
 <html>
 <head>
+    <style>
+        .ativo{
+            color:yellow;
+        }
+    </style>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 </head>
 <body>
 
@@ -29,6 +35,11 @@ $tipos = $control->selectAtivo();
 
     <label><?=$tipo->getNome()?>:</label><br>
     <input class="ran" data-id="<?=$tipo->getCod_tipo_avaliacao()?>" type="range" id="nota<?=$tipo->getCod_tipo_avaliacao()?>" name="nota" value="0" max="5"><br>
+    <i id="star1<?=$tipo->getCod_tipo_avaliacao()?>" class="far fa-star"></i>
+    <i id="star2<?=$tipo->getCod_tipo_avaliacao()?>" class="far fa-star"></i>
+    <i id="star3<?=$tipo->getCod_tipo_avaliacao()?>" class="far fa-star"></i>
+    <i id="star4<?=$tipo->getCod_tipo_avaliacao()?>" class="far fa-star"></i>
+    <i id="star5<?=$tipo->getCod_tipo_avaliacao()?>" class="far fa-star"></i><br>
     <input type="number" id="res<?=$tipo->getCod_tipo_avaliacao()?>" value="0"><br>
 
     <?php endforeach; ?>
@@ -40,6 +51,14 @@ $tipos = $control->selectAtivo();
         var id = $(this).data("id");
         var nota = $(this).val();
         $("#res"+id).val(nota);
+        if($("#star"+nota+id).hasClass("ativo")){
+
+            $("#star"+nota+id).removeClass("fas fa-star ativo");
+            $("#star"+nota+id).addClass("far fa-star");
+
+        }else{
+            $("#star"+nota+id).addClass("fas fa-star ativo");
+        }
     });
 </script>
 </html>
