3ca3f2b01ce49f412102034f9933a55d1c00bee3
arthur <arthurhaguiuda@gmail.com>
2018-10-01 14:54:58 -0300

    update endereco

--- home/controler/controlEndereco.php
+++ home/controler/controlEndereco.php
@@ -29,6 +29,32 @@
             }
         }
 
+        function update($endereco){
+            try{
+                $stmt=$this->pdo->prepare("UPDATE endereco SET rua=:rua, numero=:numero, cep=:cep, complemento=:complemento, bairro=:bairro, cliente=:cliente WHERE cod_endereco=:cod_endereco;");
+                $stmt->bindParam(":rua", $endereco->getRua(), PDO::PARAM_STR);
+                $stmt->bindParam(":numero",$endereco->getNumero(), PDO::PARAM_INT);
+                $stmt->bindParam(":cep", $endereco->getCep(), PDO::PARAM_STR);
+                $stmt->bindParam(":complemento", $endereco->getComplemento(), PDO::PARAM_STR);
+                $stmt->bindParam(":bairro", $endereco->getBairro(), PDO::PARAM_STR);
+                $stmt->bindParam(":cliente", $endereco->getCliente(), PDO::PARAM_INT);
+                $stmt->bindParam(":cod_endereco",$endereco->getCodEndereco(),PDO::PARAM_INT);
+
+                $executa=$stmt->execute();
+
+                if ($executa) {
+                    return 1;
+                }else {
+                    return -1;
+                }
+
+            }catch(PDOException $e){
+
+                echo $e->getMessage();
+
+            }
+        }
+
         function __construct($pdo){
             $this->pdo=$pdo;
         }
