1ace3fb86c6794320b65781678add9e52a91a808
Vitor Baldacin <vitormatheussb@gmail.com>
2018-10-02 11:05:02 -0300

    mudanças de imagens e cores do cardápio e acrécimo da área de médias(incompleta) da avaliação

---
+++ admin/ajax/media-avaliacao-tabela.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT']."/config.php";
+include_once CONTROLLERPATH."/seguranca.php";
+include_once CONTROLLERPATH."/controlTipoAvaliacao.php";
+include_once MODELPATH."/tipo_avaliacao.php";
+protegePagina();
+
+$controle = new controlerTipoAvaliacao($_SG['link']);
+$tipos = $controle->select();
+$permissao =  json_decode($usuarioPermissao->getPermissao());
+if(in_array('avaliacao', $permissao)){
+
+    echo "<table class='table table-responsive' id='tbCardapio' style='text-align = center;'>
+		<thead>
+			<h1 class=\"page-header\">Lista de tipos de avaliações:</h1>
+            <tr>
+                <th width='20%' style='text-align: center;'>Id</th>
+	    		<th width='20%' style='text-align: center;'>Nome</th>
+                <th width='20%' style='text-align: center;'>Ativo</th>
+                <th width='20%' style='text-align: center;'>Média</th>
+	        </tr>
+        <tbody>";
+    
+    foreach($tipos as &$tipo){
+        $mensagem = "Tipo de avaliação excluído com sucesso!";
+        $titulo = "Excluir";
+        echo "<tr name='resutaldo' id='status".$tipo->getCod_tipo_avaliacao()."'>
+                <td style='text-align: center;' name='id'>".$tipo->getCod_tipo_avaliacao()."</td>
+                <td style='text-align: center;' name='nome'>".$tipo->getNome()."</td>
+                <td style='text-align: center;' name='ativo'>".(($tipo->getFlag_ativo() == 1) ? "sim" : "nao")."</td>
+                <td style='text-align: center;' name='media'>0</td>
+                </tr>";
+    }
+
+}
+echo "</tbody></table>";
+
+?>
--- admin/view/admin/mediaAvaliacao.php
+++ admin/view/admin/mediaAvaliacao.php
@@ -1,15 +1,18 @@
 <?php
-    include_once $_SERVER['DOCUMENT_ROOT']."/config.php";
-    include_once CONTROLLERPATH."/controlUsuario.php";
-    include_once MODELPATH."/usuario.php";
-    include_once CONTROLLERPATH."/seguranca.php";
-    include_once CONTROLLERPATH."/controlTipoAvaliacao.php";
-    include_once MODELPATH."/tipo_avaliacao.php";
-    $_SESSION['permissaoPagina']=0;
-    protegePagina();
-    $controleUsuario = new controlerUsuario($_SG['link']);
-    $usuarioPermissao = $controleUsuario->select($_SESSION['usuarioID'], 2);
+include_once $_SERVER['DOCUMENT_ROOT']."/config.php";
+include_once CONTROLLERPATH."/controlUsuario.php";
+include_once MODELPATH."/usuario.php";
+include_once CONTROLLERPATH."/seguranca.php";
+include_once CONTROLLERPATH."/controlTipoAvaliacao.php";
+include_once MODELPATH."/tipo_avaliacao.php";
+$_SESSION['permissaoPagina']=0;
+protegePagina();    
+
+$controleUsuario = new controlerUsuario($_SG['link']);
+$usuarioPermissao = $controleUsuario->select($_SESSION['usuarioID'], 2);
+
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -140,30 +143,15 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-                <?php include "../../ajax/tipo-avaliacao-tabela.php";?>
+                <?php include "../../ajax/media-avaliacao-tabela.php";?>
             </div>
         </div>
     </div>
     <?php include VIEWPATH."/rodape.html" ?>
     <script src="../../js/alert.js"></script>
+    
     <script type="text/javascript">
-       function removeTipoAvaliacao(cod){
-            msgConfirmacao('Confirmação','Deseja Realmente remover o tipo avaliação ?',
-                function(linha){
-                    alert(cod);
-                    var url ="../../ajax/excluir-tipo-avaliacao.php?cod="+cod;
-                    $.get(url, function(dataReturn) {
-                        if (dataReturn > 0) {
-                            msgGenerico("Excluir!","Tipo avaliação excluido com sucesso!",1,function(){});
-                            $("#status"+cod).remove();
-                        }else{
-                            msgGenerico("Erro!","Não foi possível excluir o tipo avaliação!",2,function(){});
-                        }
-                    });  
-                },
-                function(){}
-            );
-        }
     </script>
+    
 </body>
 </html>
