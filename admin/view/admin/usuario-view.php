7330776bd629f01fd39e4716e74571f0d52fb5f7
FelipeDaMaiaBueno <felipe_mbsti@hotmail.com>
2019-07-30 09:05:23 -0300

    Alteração permissões de usuário

--- admin/view/admin/usuario-view.php
+++ admin/view/admin/usuario-view.php
@@ -294,7 +294,16 @@
                                     </li>
                                     <li class="dropdown">
                                     <a href="enderecoLista.php">Endereços</a>
-                                </li>  
+                                    </li> 
+                                    <li class="dropdown">
+                                    <!--/.Mudar aqui -->
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pedidos Whatsapp <span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="pedidoWpp.php">Novo Pedido</a></li>
+                                        <li><a href="pedidoWppLista.php">Listar Pedidos</a></li>
+                                    </ul>
+                                </li>
+
 
                                 </ul>
 
