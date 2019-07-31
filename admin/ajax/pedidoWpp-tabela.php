5c7e7427de1b9998ff90a5b333138b88ad1a26a2
FelipeDaMaiaBueno <felipe_mbsti@hotmail.com>
2019-07-31 10:46:13 -0300

    Alterações finais

--- admin/ajax/pedidoWpp-tabela.php
+++ admin/ajax/pedidoWpp-tabela.php
@@ -74,7 +74,7 @@ if(in_array('pedidoWpp', $permissao)){
 				<td style='text-align: center;' name='status'>".$pedido->getStatus()."</td>
 				<td style='text-align: center;' name='rua'>".$pedido->rua."</td>
 				<td style='text-align: center;' name='numero'>".$pedido->numero."</td>
-				<td style='text-align: center;' name='editar'><a style='font-size: 20px;' href='itemLista.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-kionux'><i class='fa fa-edit'></i>Itens</button></a></td>
+				<td style='text-align: center;' name='editar'><a style='font-size: 20px;' href='itemListaWpp.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-kionux'><i class='fa fa-edit'></i>Itens</button></a></td>
                 <td style='text-align: center;' name='imprime'><a style='font-size: 20px;' onclick=\"alterarStatus(".$pedido->getCod_pedido_wpp().",2)\"><button class='btn btn-kionux'><i class='fa fa-print'></i>Imprimir</button></a></td>
                 <td style='text-align: center;' name='delivery'><a style='font-size: 20px;'><button onclick=\"alterarStatus(".$pedido->getCod_pedido_wpp().",3)\" class='btn btn-kionux delivery'><i class='fa fa-truck'></i>Delivery</button></a></td>
             </tr>";
--- admin/view/admin/usuario.php
+++ admin/view/admin/usuario.php
@@ -509,14 +509,13 @@
 
                                     </li>
 
-                                    <li class="dropdown">
-                                    <!--/.Mudar aqui -->
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pedidos Whatsapp <span class="caret"></span></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="pedidoWpp.php">Novo Pedido</a></li>
-                                        <li><a href="pedidoWppLista.php">Listar Pedidos</a></li>
-                                    </ul>
+                                    <li>
+                                        <label>
 
+                                            <input type="checkbox" id="permissaoWpp" name="13permissao" value="pedidoWpp">Permissão Whatsapp
+
+                                        </label>
+                                    </li>
 
                                 </ul>
 
