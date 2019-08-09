9d856e5fb79d0603bfc97c065e71a8cab7e21fc4
matheus <matheus_hg95@hotmail.com>
2019-08-09 13:38:36 -0300

    merge

--- admin/index.php
+++ admin/index.php
@@ -4,9 +4,9 @@
 
 <html lang="pt-br">
 
-	<META http-equiv="refresh" content="0;URL=http://delioncafe.com.br/admin/view/login.html">
+	<!-- <META http-equiv="refresh" content="0;URL=http://delioncafe.com.br/admin/view/login.html"> -->
 
-	<!-- <META http-equiv="refresh" content="0;URL=http://delion.test/admin/view/login.html"> -->
+	<META http-equiv="refresh" content="0;URL=http://delion.test/admin/view/login.html">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 
