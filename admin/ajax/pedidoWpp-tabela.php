87ece8229ec96a58ff51ffcadd06f73f3ebc6da2
FelipeDaMaiaBueno <felipe_mbsti@hotmail.com>
2019-08-09 13:37:36 -0300

    Alterações pedidas pelo Thiago

--- admin/ajax/pedidoWpp-tabela.php
+++ admin/ajax/pedidoWpp-tabela.php
@@ -47,14 +47,15 @@ if(in_array('pedidoWpp', $permissao)){
 	<thead>
 		<h1 class=\"page-header\">Lista de Pedidos</h1>
 		<div class=\"pull-right\">
-			<a href=\"pedidoWpp.php\" class=\"btn btn-kionux\"><i class=\"fa fa-arrow-left\"></i> Voltar</a>
+			<a href=\"pedidoWpp.php\" class=\"btn btn-primary\"><i class=\"fa fa-arrow-left\"></i> Voltar</a>
 		</div class=\"pull-right\">
 		<tr>
-    		<th width='8%' style='text-align: center;'>Data</th>
+			<th width='10%' style='text-align: center;'>Código Pedido</th>
+			<th width='8%' style='text-align: center;'>Data</th>
+			<th width='8%' style='text-align: center;'>Hora Pedido</th>
 			<th width='10%' style='text-align: center;'>Nome do cliente</th>
 			<th width='10%' style='text-align: center;'>Telefone do cliente</th>
 			<th width='8%' style='text-align: center;'>Valor total</th>
-			<th width='5%' style='text-align: center;'>Status</th>
 			<th width='15%' style='text-align: center;'>Rua</th>
 			<th width='8%' style='text-align: center;'>Número</th>
         </tr>
@@ -64,17 +65,18 @@ if(in_array('pedidoWpp', $permissao)){
 				$mensagem='Cliente excluído com sucesso!';
 				$titulo='Excluir';
 				echo "<tr name='resultado' id='status".$pedido->getCod_pedido_wpp()."'>
+					<td style='text-align: center;' name='cliente'>".$pedido->getCod_pedido_wpp()."</td>
 					<td style='text-align: center;' name='data'>".$pedido->getData()->format('d/m/Y')."</td>
+					<td style='text-align: center;' name='data'>".$pedido->getData()->format('H:i')."</td>
 					<td style='text-align: center;' name='cliente'>".$pedido->getCliente_wpp()."</td>
 					<td style='text-align: center;' name='telefone'>".$pedido->telefone."</td>
 					<td style='text-align: center;' name='valor'>"." R$ ".$pedido->getValor()."</td>
-					<td style='text-align: center;' name='status'>".$pedido->getStatus()."</td>
 					<td style='text-align: center;' name='rua'>".$pedido->rua."</td>
 					<td style='text-align: center;' name='numero'>".$pedido->numero."</td>
-					<td style='text-align: center;' name='editar'><a style='font-size: 10px;' href='itemListaWpp.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-kionux'><i class='fa fa-edit'></i>Itens</button></a></td>
-					<td style='text-align: center;' name='imprime'><a style='font-size: 10px;'><button class='btn btn-kionux btn-print' data-toggle='modal' data-target='#modalPedido".$pedido->getCod_pedido_wpp()."' data-id='".$pedido->getCod_pedido_wpp()."'><i class='fa fa-print'></i>Imprimir</button></a></td>
-					<td style='text-align: center;' name='delivery'><a style='font-size: 10px;'><button onclick=\"erroDelivery(".$pedido->getStatus().")\" class='btn btn-kionux delivery'><i class='fa fa-truck'></i>Delivery</button></a></td>
-					<td style='text-align: center;' name='detalhes'><a style='font-size: 10px;' ' href='descPage.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-kionux'><i class='fa fa-info'></i>Detalhes</button></a></td>
+					<td style='text-align: center;' name='editar'><a style='font-size: 10px;' href='itemListaWpp.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-primary'><i class='fa fa-edit'></i>Itens</button></a></td>
+					<td style='text-align: center;' name='imprime'><a style='font-size: 10px;'><button class='btn btn-primary' data-toggle='modal' data-target='#modalPedido".$pedido->getCod_pedido_wpp()."' data-id='".$pedido->getCod_pedido_wpp()."'><i class='fa fa-print'></i>Imprimir</button></a></td>
+					<td style='text-align: center;' name='delivery'><a style='font-size: 10px;'><button onclick=\"erroDelivery(".$pedido->getStatus().")\" class='btn btn-danger' disable><i class='fa fa-truck'></i>Delivery</button></a></td>
+					<td style='text-align: center;' name='detalhes'><a style='font-size: 10px;' ' href='descPage.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-primary'><i class='fa fa-info'></i>Detalhes</button></a></td>
 					</tr>";
 		}
 	}	//Mudar o botao delivery e limitar as opções
@@ -84,17 +86,18 @@ if(in_array('pedidoWpp', $permissao)){
 				$mensagem='Cliente excluído com sucesso!';
 				$titulo='Excluir';
 				echo "<tr name='resultado' id='status".$pedido->getCod_pedido_wpp()."'>
+					<td style='text-align: center;' name='cliente'>".$pedido->getCod_pedido_wpp()."</td>
 					<td style='text-align: center;' name='data'>".$pedido->getData()->format('d/m/Y')."</td>
+					<td style='text-align: center;' name='data'>".$pedido->getData()->format('H:i')."</td>
 					<td style='text-align: center;' name='cliente'>".$pedido->getCliente_wpp()."</td>
 					<td style='text-align: center;' name='telefone'>".$pedido->telefone."</td>
 					<td style='text-align: center;' name='valor'>"." R$ ".$pedido->getValor()."</td>
-					<td style='text-align: center;' name='status'>".$pedido->getStatus()."</td>
 					<td style='text-align: center;' name='rua'>".$pedido->rua."</td>
 					<td style='text-align: center;' name='numero'>".$pedido->numero."</td>
-					<td style='text-align: center;' name='editar'><a style='font-size: 10px;' href='itemListaWpp.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-kionux'><i class='fa fa-edit'></i>Itens</button></a></td>
-					<td style='text-align: center;' name='imprime'><a style='font-size: 10px;' ><button onclick=\"erroPrint(".$pedido->getStatus().")\" class='btn btn-kionux'><i class='fa fa-print'></i>Imprimir</button></a></td>
-					<td style='text-align: center;' name='delivery'><a style='font-size: 10px;'><button onclick=\"alterarStatusDelivery(".$pedido->getCod_pedido_wpp().",3)\" class='btn btn-kionux delivery'><i class='fa fa-truck'></i>Delivery</button></a></td>
-					<td style='text-align: center;' name='detalhes'><a style='font-size: 10px;' ' href='descPage.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-kionux'><i class='fa fa-info'></i>Detalhes</button></a></td>
+					<td style='text-align: center;' name='editar'><a style='font-size: 10px;' href='itemListaWpp.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-primary'><i class='fa fa-edit'></i>Itens</button></a></td>
+					<td style='text-align: center;' name='imprime'><a style='font-size: 10px;' ><button class='btn btn-danger'><i class='fa fa-print' disable></i>Imprimir</button></a></td>
+					<td style='text-align: center;' name='delivery'><a style='font-size: 10px;'><button onclick=\"alterarStatusDelivery(".$pedido->getCod_pedido_wpp().",3)\" class='btn btn-primary'><i class='fa fa-truck'></i>Delivery</button></a></td>
+					<td style='text-align: center;' name='detalhes'><a style='font-size: 10px;' ' href='descPage.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-primary'><i class='fa fa-info'></i>Detalhes</button></a></td>
 					</tr>";
 		}
 	}	//Mudar o botao delivery e limitar as opções
@@ -104,17 +107,18 @@ if(in_array('pedidoWpp', $permissao)){
 			$mensagem='Cliente excluído com sucesso!';
 			$titulo='Excluir';
 			echo "<tr name='resultado' id='status".$pedido->getCod_pedido_wpp()."'>
+				<td style='text-align: center;' name='cliente'>".$pedido->getCod_pedido_wpp()."</td>
 				<td style='text-align: center;' name='data'>".$pedido->getData()->format('d/m/Y')."</td>
+				<td style='text-align: center;' name='data'>".$pedido->getData()->format('H:i')."</td>
 				<td style='text-align: center;' name='cliente'>".$pedido->getCliente_wpp()."</td>
 				<td style='text-align: center;' name='telefone'>".$pedido->telefone."</td>
 				<td style='text-align: center;' name='valor'>"." R$ ".$pedido->getValor()."</td>
-				<td style='text-align: center;' name='status'>".$pedido->getStatus()."</td>
 				<td style='text-align: center;' name='rua'>".$pedido->rua."</td>
 				<td style='text-align: center;' name='numero'>".$pedido->numero."</td>
-				<td style='text-align: center;' name='editar'><a style='font-size: 10px;' href='itemListaWpp.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-kionux'><i class='fa fa-edit'></i>Itens</button></a></td>
-				<td style='text-align: center;' name='imprime'><a style='font-size: 10px;' ><button onclick=\"erroDelivery(".$pedido->getStatus().")\" class='btn btn-kionux btn-print'><i class='fa fa-print'></i>Imprimir</button></a></td>
-				<td style='text-align: center;' name='delivery'><a style='font-size: 10px;'><button onclick=\"erroDelivery(".$pedido->getStatus().")\" class='btn btn-kionux delivery'><i class='fa fa-truck'></i>Delivery</button></a></td>
-				<td style='text-align: center;' name='detalhes'><a style='font-size: 10px;' ' href='descPage.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-kionux'><i class='fa fa-info'></i>Detalhes</button></a></td>
+				<td style='text-align: center;' name='editar'><a style='font-size: 10px;' href='itemListaWpp.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-primary'><i class='fa fa-edit'></i>Itens</button></a></td>
+				<td style='text-align: center;' name='imprime'><a style='font-size: 10px;' ><button class='btn btn-danger'><i class='fa fa-print'></i>Imprimir</button></a></td>
+				<td style='text-align: center;' name='delivery'><a style='font-size: 10px;'><button class='btn btn-danger'><i class='fa fa-truck'></i>Delivery</button></a></td>
+				<td style='text-align: center;' name='detalhes'><a style='font-size: 10px;' ' href='descPage.php?cod=".$pedido->getCod_pedido_wpp()."'><button class='btn btn-primary'><i class='fa fa-info'></i>Detalhes</button></a></td>
 				</tr>";
 		}
 	}	//Mudar o botao delivery e limitar as opções
@@ -126,11 +130,12 @@ if(in_array('pedidoWpp', $permissao)){
 	<thead>
 		<h1 class=\"page-header\">Lista de Pedidos</h1>
 		<tr>
-    		<th width='25%' style='text-align: center;'>Data</th>
+			<th width='10%' style='text-align: center;'>Código Pedido</th>
+			<th width='25%' style='text-align: center;'>Data</th>
+			<th width='8%' style='text-align: center;'>Hora Pedido</th>
 			<th width='15%' style='text-align: center;'>Nome do cliente</th>
 			<th width='30%' style='text-align: center;'>Telefone do cliente</th>
 			<th width='15%' style='text-align: center;'>Valor total</th>
-			<th width='15%' style='text-align: center;'>Status</th>
         </tr>
 	<tbody>";
 	foreach ($pedidos as &$pedido) {
@@ -164,17 +169,11 @@ if(in_array('pedidoWpp', $permissao)){
 							<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>
 							<br><h4 class=\"modal-title\" id=\"exampleModalLabel\" style=\"text-align:center\">Dados para Impressão</h4>
 						</div>
-<<<<<<< HEAD
-						<div class=\"modal-body\" style=\"text-align:center\" id=\"divPrin\" >
-							<form>
-									<div class=\"form-group\">
-=======
 						<div class=\"modal-body\" style=\"text-align:center\" id=\"divPrin\">
 							<form>";
 
 							
 							echo "<div class=\"form-group\">
->>>>>>> a4dafa48604b8b10a32ceb2776d84d8f76224d97
 									<label for=\"recipient-name\" class=\"control-label\">----------------------------------------------------------</label>
 									<br><label for=\"recipient-name\" class=\"control-label\">Restaurante: DELION.O </label>
 										<br><label for=\"message-text\" class=\"control-label\">"."Data: ".$pedido->getData()->format('d/m/Y')." ".$pedido->getData()->format('H:i')."</label>
