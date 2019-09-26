fd995e2cd146dace4d7e8ea3d3df073bfc61ddd9
Isshak Darwich <isshak@corp.kionux.com.br>
2019-09-26 14:57:01 -0300

    Modificação 'autocomplete index'

--- home/index.php
+++ home/index.php
@@ -5,18 +5,20 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Delion Café</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-	<link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 </head>
 
 <body>
 
 	<div></div>
 
-    <div>
+    <div style="margin: auto; margin-top:300px; width:50%;">
 		
 		<div>
 			<div id="locationField" style="display:inline-block;">
-				<input id="autocomplete" placeholder="Digite seu endereço ou CEP" type="text" size="60"/>
+				<input type="text" class="form-control" id="autocomplete" placeholder="Digite seu endereço ou CEP" type="text" size="45"/>
 			</div>
 			<div  style="display:inline-block;">
 				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
@@ -24,7 +26,9 @@
 				</div>
 				<div style="display:inline-block;">
 					<button type="button" class="btn btn-success" onclick="getLocation()">
-						<span>Me Localizar</span>
+						<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
+						Me Localizar
+					</button>
 				</div>
 			</div>
 		</div>
@@ -43,43 +47,52 @@
 			<div class="modal-content">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="myModalLabel">Modal title</h4>
+					<h4 style="text-align:center;" class="modal-title" id="myModalLabel">Falta pouco para você ter em sua casa nossas delícias!<br>Complete seu cadastro para ter acesso ao nosso site.</h4>
 				</div>
 				<div class="modal-body">
+				<form>
 					<div id="address">
-						<div>
-							<div class="label">UF</div>
-							<div class="slimField"><input class="field" id="administrative_area_level_1" /></div>
-							
-							<div class="label">Cidade</div>
-							<div class="wideField" colspan="3"><input class="field" id="administrative_area_level_2" /></div>
-							
-						</div>
-						<div>
-							<div class="label">CEP</div>
-							<div class="wideField"><input class="field" id="postal_code" /></div>
-							
-							<div class="label">Bairro</div>
-							<div class="wideField"><input class="field" id="sublocality_level_1" /></div>
-							
-						</div>
-						<div>
-							<div class="label">Endereço</div>
-							<div class="wideField" colspan="6"><input class="field" id="route" /></div>
-						</div>
-						<div>        
-							<div class="slimField">Nº<input class="field" id="street_number" /></div>
-							
-							<div class="slimField">
-								Complemento
-								<input class="field" />
+						<div class="row">
+							<div class="col-sm-3">
+								<label>UF</label>
+								<input type="text" class="form-control" id="administrative_area_level_1" />
 							</div>
-							
+							<div class="col-sm-9">
+								<label>Cidade</label>
+								<input type="text" class="form-control" id="administrative_area_level_2" />
+							</div>
 						</div>
-						
-						<div>
-							<div class="label">Seria legal se Ref</div>
-							<div class="wideField" colspan="6"><input class="field" /></div>
+						<div class="row">
+							<div class="col-sm-4">
+								<label>CEP</label>
+								<input type="text" class="form-control" id="postal_code" />
+							</div>
+							<div class="col-sm-8">
+								<label>Bairro</label>
+								<input type="text" class="form-control" id="sublocality_level_1" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-12">
+								<label>Endereço</label>
+								<input type="text" class="form-control" id="route" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-4">
+								<label>Número</label>
+								<input type="text" class="form-control" id="street_number" />
+							</div>
+							<div class="col-sm-8">
+								<label>Complemento</label>
+								<input type="text" class="form-control" id="sublocality_level_1" />
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-12">
+								<label>Alguma referência para localização?</label>
+								<input type="text" class="form-control" id="complemento" />
+							</div>
 						</div>
 					</div>
 				</div>
@@ -101,8 +114,6 @@
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
 <script
 src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAS7HedlAWWAMuzXlS8boXxNIC5RJFUH-A&libraries=places&callback=initAutocomplete"
 async defer></script>
@@ -111,7 +122,7 @@ async defer></script>
 <script>
 
 	$('#myModal').on('shown.bs.modal', function () {
-		$('sublocality_level_1').focus();
+		$('#street_number').focus();
 	});
 
 </script>
