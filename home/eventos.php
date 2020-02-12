677fcfeafd75eba2e6f350ad6769974595a60a4b
matheus <matheus_hg95@hotmail.com>
2020-02-12 15:23:21 -0300

    ajuste textual tela evento

--- home/eventos.php
+++ home/eventos.php
@@ -54,7 +54,7 @@ session_start();
 		<div class="row solicitacao visible-xs-* visible-sm-* visible-md-* visible-lg-* visible-xl-* form-group">
 
 			<div class="titulo-eventos">
-				<h2><i class="fas fa-bookmark"></i> Informações do Evento </h2>
+				<h2><i class="fas fa-bookmark"></i> Solicitações de orçamento </h2>
 			</div>
 
 			<div class="col-sm campos-evento">
