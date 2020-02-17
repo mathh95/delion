8d845573a17169766ac7046524af4cc1e3993188
matheus <matheus_hg95@hotmail.com>
2020-02-17 14:16:08 -0300

    masked input tela alterar fornecedor

--- admin/view/admin/fornecedor-view.php
+++ admin/view/admin/fornecedor-view.php
@@ -192,8 +192,30 @@
         
 
         <?php include VIEWPATH."/rodape.html" ?>
+
+        <script type="text/javascript" src="../../js/maskedinput.js"></script>
         
         <script>
+
+
+        $(document).ready(function(){
+            $("#cnpj").mask("99.999.999/9999-99");
+        });
+
+        $("input.telefone").mask("(99) ?9 9999-9999").focusout(function (event) {  
+
+            var target, phone, element;  
+            target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
+            phone = target.value.replace(/\D/g, '');
+            element = $(target);  
+            element.unmask();  
+
+            if(phone.length > 10) {  
+                element.mask("(99) 99999-999?9");  
+            } else {  
+                element.mask("(99) 9999-9999?9");  
+            }  
+        });
         
         $(document).ready(function() {
         
